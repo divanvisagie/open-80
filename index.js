2ce215bf3378e07ce2c95bba1527c6f50fd6f9b8
Divan Visagie <me@dvisagie.com>
2018-05-06 14:47:46 +0200

    make it a command

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+#! /usr/bin/env node
 'use strict'
 
 const request = require('request')
 const nmap = require('node-nmap')
 const Spinner = require('cli-spinner').Spinner;
 
-function getHosts(callback) {
+function getHosts(ipString, callback) {
     let scanspinner = new Spinner('Scanning network...')
     scanspinner.start()
-    const nmapscan = new nmap.NmapScan('192.168.8.0/24', '-sS -O -p80,8080')
+    const nmapscan = new nmap.NmapScan(`${ipString}/24`, '-sS -O -p80,8080')
     nmapscan.on('complete', hosts => {
 
         console.log("Found:")
@@ -27,15 +28,18 @@ function getHosts(callback) {
         callback(filteredData);
     })
     nmapscan.on('error', error => {
-        console.log('error', error)
+        console.log('\nError:', error)
         scanspinner.stop()
+        process.exit()
     })
 
     nmapscan.startScan()
 }
 
 
-getHosts(hosts => {
+const ip = '192.168.8.0'
+
+getHosts(ip,hosts => {
     console.log('\n')
     hosts.forEach(host => {
         let url = `http://${host.ip}:${host.port}`
